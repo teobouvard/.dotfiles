d541a49d16d367e5070dafde60256e4694558d56
Téo Bouvard <teobouvard@gmail.com>
2019-08-16 21:27:07 +0200

    add git cred store

--- install.sh
+++ install.sh
@@ -133,6 +133,7 @@ gsettings set org.cinnamon startup-animation false
 
 git config --global user.email "teobouvard@gmail.com"
 git config --global user.name "Téo Bouvard"
+git config --global credential.helper store
 git config --global alias.lg "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 success "git is set up"
 
