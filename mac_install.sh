d1ffecfbcd318ce82a7a11c93cad62dd29cfb8e9
Téo Bouvard <teobouvard@gmail.com>
2019-08-17 23:28:23 +0200

    posix path

--- mac_install.sh
+++ mac_install.sh
@@ -23,8 +23,7 @@ success "packages installed"
 ###     DESKTOP APPEARANCE          ###
 #######################################
 
-mkdir -p $HOME/Pictures/Wallpapers/
-sudo wget -qO $HOME/Pictures/Wallpapers/dragon.jpg https://www.nasa.gov/sites/default/files/thumbnails/image/iss058e027197.jpg
-sudo wget -qO $HOME/Pictures/Wallpapers/flow.jpg https://dubaiastronomy.com/wp-content/uploads/2019/04/art-artistic-background-1020315.jpg
+sudo wget -qO /Library/Desktop Pictures/dragon.jpg https://www.nasa.gov/sites/default/files/thumbnails/image/iss058e027197.jpg
+sudo wget -qO /Library/Desktop Pictures/flow.jpg https://dubaiastronomy.com/wp-content/uploads/2019/04/art-artistic-background-1020315.jpg
 
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "$HOME/Pictures/Wallpapers/dragon.jpg" '
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/dragon.jpg" '
